99d283134532ec16f08ef617fee3808dab6cac93
Thomas Triadi <netson99@gmail.com>
2014-01-14 16:14:27 +0700

    slimkat build

---
+++ slim.mk
@@ -0,0 +1,16 @@
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/motorola/droid2we/droid2we.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := droid2we
+PRODUCT_NAME := slim_droid2we
+PRODUCT_BRAND := verizon
+PRODUCT_MODEL := DROID2 Global
+PRODUCT_MANUFACTURER := Motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=droid2we_vzw BUILD_FINGERPRINT=verizon/droid2we_vzw/cdma_droid2we:2.3.4/4.5.1_57_D2GA-59/120117:user/release-keys PRIVATE_BUILD_DESC="cdma_droid2we-user 2.3.4 4.5.1_57_D2GA-59 120117 release-keys"
+
+TARGET_BOOTANIMATION_NAME := 480
